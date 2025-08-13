47585fef6e569dadaf4924510303268409f63ca1
Mukund Chandak <chandakmukund@yahoo.co.in>
2025-08-13 18:04:42 +0530

    require dotenv config

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const connectDB = async () => {
+    console.log(process.env.MONGO_DB_SECRET)
     await mongoose.connect(process.env.MONGO_DB_SECRET);
 };
 
